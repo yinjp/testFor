6af3bf075ec36c5c515b1c11330f4f5a41a2793f
yinjiaoping <yinjiaoping@163.com>
2016-03-07 17:18:29 +0800

    first commit

---
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,23 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+
+	@RequestMapping(value = "name",method = RequestMethod.GET)
+	public String printWelcome(@RequestParam("aaa")String aaa) {
+		System.out.println("test----------"+aaa);
+		return "hello1";
+	}
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+	<h1>${message}</h1>
+<form action="/name" >
+	<input name="aaa" type="text"/>
+	<button type="submit">ok</button>
+</form>
+</body>
+</html>
